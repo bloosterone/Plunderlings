4c3d8d07f7402ede1ff9c7a51511e03d11bcbf71
diegoobls <xldiegoo@gmail.com>
2023-11-02 18:09:18 -0300

    se agrega nueva funcionalidad botones

--- js/index.js
+++ js/index.js
@@ -52,3 +52,56 @@ const grande = document.querySelector('.menu__contenedor-grande');
 	
 					
 				});
+
+const controlador_vertical = document.querySelector('.controlador_vertical');
+const botones = document.querySelectorAll('.btn');
+let cont=0;
+
+				botones[0].addEventListener('click', ()=>{
+					console.log("boton 1");
+					botones.forEach( (cadaPunto, i)=>{
+						botones[i].classList.remove('btn_selec');
+					} );
+					botones[0].classList.add("btn_selec");
+					cont=0;
+	
+						// if(contador== 25){
+						// 	contador=-75;
+						// 	controlador_vertical.style.transform=` translateY(${contador}%)`;
+						// }
+					controlador_vertical.style.transform=` translateY(${cont}%)`;
+					
+
+				});
+				botones[2].addEventListener('click', ()=>{
+					console.log("boton 2");
+					botones.forEach( (cadaPunto, i)=>{
+						botones[i].classList.remove('btn_selec');
+					} )
+					botones[2].classList.add("btn_selec");
+					
+					cont = -25;
+					console.log(cont)
+					controlador_vertical.style.transform=` translateY(${cont}%)`;
+				});
+				botones[3].addEventListener('click', ()=>{
+					console.log("boton 3");
+					botones.forEach( (cadaPunto, i)=>{
+						botones[i].classList.remove('btn_selec');
+					} )
+					botones[3].classList.add("btn_selec");
+					cont = -50;
+					console.log(cont)
+					controlador_vertical.style.transform=` translateY(${cont}%)`;
+				});
+				botones[1].addEventListener('click', ()=>{
+					console.log("boton 4");
+					botones.forEach( (cadaPunto, i)=>{
+						botones[i].classList.remove('btn_selec');
+					} )
+					botones[1].classList.add("btn_selec");
+					cont = -75;
+					console.log(cont)
+					controlador_vertical.style.transform=` translateY(${cont}%)`;
+
+				});
